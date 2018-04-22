f32e9fc70a4b449d2f44d840d453e2ab8b7df84f
nhojyn <38599261+nhojyn@users.noreply.github.com>
2018-04-22 13:21:48 -0700

    Add files via upload

---
+++ Mob.java
@@ -0,0 +1,56 @@
+import javafx.geometry.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
+import javafx.scene.layout.*;
+
+import javafx.scene.transform.Rotate;
+
+//eventually this class should be abstract
+public class Mob extends Pane{
+	
+	//fields
+	Rectangle body;
+	Rectangle front; //indicates where front of mob is
+	double speedModifier = 1.0; //total distance that it walks per cycle
+	
+	public Mob(){
+		body = new Rectangle(0,0,50,50);
+		body.setFill(Color.BLUE);
+		getChildren().add(body);
+		
+		front = new Rectangle(body.getWidth()/2-2.5, body.getHeight()-5, 5,5);
+		getChildren().add(front);
+		
+	}
+	
+	//setters and getters
+	public double getLocX(){
+		return getLayoutX();
+	}
+	public double getLocY(){
+		return getLayoutY();
+	}
+	
+	//methods
+	public void chase(Player p){
+		double px = p.getLocX()- getLayoutX();
+		double py = p.getLocY()- getLayoutY();
+		
+		double distance = Math.pow(((px*px) + (py*py)),0.5);
+		
+		move(px*speedModifier/distance,py*speedModifier/distance);
+		
+		rotate(p.getLocX(),p.getLocY());
+	}
+	
+	private void move(double x, double y){
+		setLayoutX(getLayoutX()+x);
+		setLayoutY(getLayoutY()+y);
+	}
+	
+	private void rotate(double x, double y){
+		double angle = Math.atan2(getLayoutY()-y,getLayoutX()-x);
+		getTransforms().clear();
+		getTransforms().add(new Rotate(Math.toDegrees(angle)+90));
+	}
+}
---
+++ Swarm.java
@@ -0,0 +1,66 @@
+import javafx.geometry.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
+import javafx.scene.layout.*;
+import java.util.*;
+
+//swarm has all the mobs and stuff
+public class Swarm{
+	
+	//fields
+	ArrayList<Mob> swarm = new ArrayList<Mob>();
+	int numMobs;
+	
+	//constructors
+	public Swarm(int n){
+		numMobs = n;
+	}
+	
+	//setters and getters
+	public ArrayList<Mob> getSwarm(){return swarm;}
+	
+	
+	//methods
+	public void spawnSwarm(Pane main){
+		for(int i = 0; i < numMobs; i++){
+			double spawnX = 0;
+			double spawnY = 0;
+			
+			int spawnPos = (int)(Math.random()*4); //0=north, 1=east, 2=south, 3=west
+			
+			switch (spawnPos){
+				case 0: spawnX = Math.random()*main.getPrefWidth();
+						break;
+				case 1: spawnY = Math.random()*main.getPrefHeight();
+						break;
+				case 2: spawnX = Math.random()*main.getPrefWidth();
+						spawnY = main.getPrefHeight();
+						break;
+				case 3: spawnY = Math.random()*main.getPrefHeight();
+						spawnX = main.getPrefWidth();
+						break;
+			}
+			
+			System.out.println(spawnX + "," + spawnY);
+			Mob temp = new Mob();
+			swarm.add(temp);
+			main.getChildren().add(temp);
+			temp.setLayoutX(spawnX);
+			temp.setLayoutY(spawnY);
+			
+		}
+		
+	}
+	
+	public void swarmPlayer(Player p){
+		for(int i = 0; i < swarm.size(); i++){
+			swarm.get(i).chase(p);
+		}
+	}
+	
+	public void resetSwarm(){
+		swarm = new ArrayList<Mob>();
+	}
+	
+	
+}
