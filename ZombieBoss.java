5ffb690ba7c5cafb8e3790b35feed3624c4d96bd
nhojyn <38599261+nhojyn@users.noreply.github.com>
2018-05-24 20:19:09 -0700

    added animations and skin to ZombieBoss

--- ZombieBoss.java
+++ ZombieBoss.java
@@ -6,35 +6,60 @@ import javafx.scene.layout.*;
 import javafx.util.Duration;
 import javafx.animation.*;
 import javafx.scene.transform.Rotate;
+import javafx.scene.image.*;
 
 public class ZombieBoss extends Boss implements TrueBounds{
 	
 	private final int MAX_COOLDOWN = 250;
 	int cooldown = 20; //it will attack every 20*MAX_COOLDOWN ms (so for 125, it shoots every 5 seconds)
-	Rectangle west,north,east,south;
 	Swarm mobs;
 	Pane pg;
 	boolean grown = false;
+	ImageView imgview;
+	ImageView enragedimg;
+	boolean enraged;
 	public ZombieBoss(Pane main, Swarm s,UserInterface ui){
 		super(ui);
 		mobs = s;
 		pg = main;
-		health = 300;
+		health = 200;
 		ui.addBossHP(health);
 		points = 200;
-		body = new Rectangle(0,0,200,200);
-		body.setFill(Color.BLUE);
+		
+		//body = new Rectangle(0,0,200,200);
+		body = new Rectangle(0,0,0,0);
+		body.setFill(Color.TRANSPARENT);
 		getChildren().add(body);
-		front = new Rectangle(0,0,200,200);
-		front.setFill(Color.BLUE);
-		west = new Rectangle(0,((Rectangle)body).getHeight()/2 -10, 20,20);
-		north = new Rectangle(((Rectangle)body).getWidth()/2-10, 0, 20,20);
-		east = new Rectangle(((Rectangle)body).getWidth()-20,((Rectangle)body).getHeight()/2 -10, 20,20);
-		south = new Rectangle(((Rectangle)body).getWidth()/2 - 10,((Rectangle)body).getHeight()-20, 20,20);
+		front = new Rectangle(0,0,0,0);
+		front.setFill(Color.TRANSPARENT);
 		middle = new Rectangle(((Rectangle)body).getWidth()/2, ((Rectangle)body).getHeight()/2, 5,5);
-		getChildren().addAll(front,middle,west,north,east,south);
+		middle.setFill(Color.TRANSPARENT);
+		getChildren().addAll(front,middle);
+		try{
+			Image img = new Image("zombie.png");
+			imgview = new ImageView(img);
+			imgview.setFitWidth(1);
+			imgview.setFitHeight(1);
+			getChildren().add(imgview);
+			Image en = new Image("zombie_1.png");
+			enragedimg = new ImageView(en);
+			enragedimg.setFitWidth(200);
+			enragedimg.setFitHeight(200);
+		}
+		catch(Exception e){
+			System.out.println("error while creating image");
+			e.printStackTrace();
+		}	
+		enraged = false;
 	}
 	
+	public void spawn(Player p){
+		super.spawn(p);
+		Timeline spawn = new Timeline(new KeyFrame(Duration.millis(10), ae -> spawnGrow()));
+		spawn.setCycleCount(100);
+		spawn.play();
+		
+	}
 	//grow and shirnk
 	public void chase(Player p){
 		if(!spawned){
@@ -93,7 +118,11 @@ public class ZombieBoss extends Boss implements TrueBounds{
 	private boolean attack3(){
 		if(mobs.getSwarm().size() <=1){
 			return false;
-		}	
+		}
+		toggleEnraged();
+		Timeline toggle = new Timeline(new KeyFrame(Duration.millis(1000),ae -> toggleEnraged()));
+		toggle.setCycleCount(1);
+		toggle.play();		
 		for(Mob m : mobs.getSwarm()){
 			m.setSpeedModifier(m.getSpeedModifier()*2);
 		}
@@ -131,13 +160,14 @@ public class ZombieBoss extends Boss implements TrueBounds{
 		front.setHeight(front.getHeight() + 1.76);
 		setLayoutX(getLayoutX()- 0.88);
 		setLayoutY(getLayoutY()- 0.88);
-		getChildren().removeAll(front,middle,west,north,east,south);
-		west = new Rectangle(0,((Rectangle)body).getHeight()/2 -10, 20,20);
-		north = new Rectangle(((Rectangle)body).getWidth()/2-10, 0, 20,20);
-		east = new Rectangle(((Rectangle)body).getWidth()-20,((Rectangle)body).getHeight()/2 -10, 20,20);
-		south = new Rectangle(((Rectangle)body).getWidth()/2 - 10,((Rectangle)body).getHeight()-20, 20,20);
+		getChildren().removeAll(front,middle);
 		middle = new Rectangle(((Rectangle)body).getWidth()/2, ((Rectangle)body).getHeight()/2, 5,5);
-		getChildren().addAll(front,middle,west,north,east,south);
+		middle.setFill(Color.TRANSPARENT);
+		getChildren().addAll(front,middle);
+		imgview.setFitWidth(front.getWidth());
+		imgview.setFitHeight(front.getHeight());
+		enragedimg.setFitWidth(front.getWidth());
+		enragedimg.setFitHeight(front.getHeight());
 	}
 	private void shrink(){
 		((Rectangle)body).setWidth(((Rectangle)body).getWidth() - 1.76);
@@ -146,13 +176,14 @@ public class ZombieBoss extends Boss implements TrueBounds{
 		front.setHeight(front.getHeight() - 1.76);
 		setLayoutX(getLayoutX()+ 0.88);
 		setLayoutY(getLayoutY()+ 0.88);
-		getChildren().removeAll(front,middle,west,north,east,south);
-		west = new Rectangle(0,((Rectangle)body).getHeight()/2 -10, 20,20);
-		north = new Rectangle(((Rectangle)body).getWidth()/2-10, 0, 20,20);
-		east = new Rectangle(((Rectangle)body).getWidth()-20,((Rectangle)body).getHeight()/2 -10, 20,20);
-		south = new Rectangle(((Rectangle)body).getWidth()/2 - 10,((Rectangle)body).getHeight()-20, 20,20);
+		getChildren().removeAll(front,middle);
 		middle = new Rectangle(((Rectangle)body).getWidth()/2, ((Rectangle)body).getHeight()/2, 5,5);
-		getChildren().addAll(front,middle,west,north,east,south);
+		middle.setFill(Color.TRANSPARENT);
+		getChildren().addAll(front,middle);
+		imgview.setFitWidth(front.getWidth());
+		imgview.setFitHeight(front.getHeight());
+		enragedimg.setFitWidth(front.getWidth());
+		enragedimg.setFitHeight(front.getHeight());
 	}
 	
 	public double getBodyHeight(){
@@ -161,5 +192,29 @@ public class ZombieBoss extends Boss implements TrueBounds{
 	
 	public double getBodyWidth(){
 		return ((Rectangle)body).getWidth();
+	}
+	
+	private void spawnGrow(){
+		((Rectangle)body).setWidth(((Rectangle)body).getWidth() + 2);
+		((Rectangle)body).setHeight(((Rectangle)body).getHeight() + 2);
+		front.setWidth(front.getWidth() + 2);
+		front.setHeight(front.getHeight() + 2);
+		setLayoutX(getLayoutX()- 1);
+		setLayoutY(getLayoutY()-1);
+		imgview.setFitWidth(front.getWidth());
+		imgview.setFitHeight(front.getHeight());
+	}
+	
+	private void toggleEnraged(){
+		enragedimg.setFitWidth(front.getWidth());
+		enragedimg.setFitHeight(front.getHeight());
+		if(enraged){
+			getChildren().remove(enragedimg);
+			enraged = false;
+		}
+		else{
+			getChildren().add(enragedimg);
+			enraged = true;
+		}
 	}
 }
