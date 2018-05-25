6aef1e8d35f6e8304e6379ecbb775e4552962d55
nickng589 <38599477+nickng589@users.noreply.github.com>
2018-05-25 00:14:35 -0700

    Add files via upload

--- RectParticle.java
+++ RectParticle.java
@@ -23,7 +23,7 @@ public class RectParticle extends Rectangle implements Animated{
 	double yVel;
 	double fade;
 	
-	RectParticle(){
+	RectParticle(Color c){
 		xVel=Math.random()*2;
 		if((int)(Math.random()*2)==0){
 			xVel=-xVel;
@@ -33,7 +33,7 @@ public class RectParticle extends Rectangle implements Animated{
 			yVel=-yVel;
 		}
 		fade=Math.random()/20+.015;
-		setFill(Color.BLUE);
+		setFill(c);
 		setWidth(20);
 		setHeight(20);
 		
