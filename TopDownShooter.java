b3b5031765f3f332e12a9a2c25426754772a6faa
alecl303 <38599448+alecl303@users.noreply.github.com>
2018-05-24 17:29:15 -0700

    Add files via upload

--- TopDownShooter.java
+++ TopDownShooter.java
@@ -297,7 +297,7 @@ public class TopDownShooter{
 		});
 
 		//initiates roundList and starts playing the rounds
-		roundList = new RoundList(playground, mobs, ui);
+		roundList = new RoundList(playground, mobs, ui,overlay);
 		Button startRounds = new Button();
 		devTools.getChildren().add(startRounds);
 		startRounds.setText("Start Rounds");
@@ -456,7 +456,11 @@ public class TopDownShooter{
 						ui.removeBossHP();
 					}
 					if(mobs.getSwarm(i) instanceof PistolMob){
-						
+						int temp = mobs.getSwarm(i).getProjectiles().size();
+						for(int b = 0; b < temp; b++){
+							playground.getChildren().remove(mobs.getSwarm(i).getProjectiles().get(0));
+							mobs.getSwarm(i).getProjectiles().remove(0);
+						}
 					}
 					int temp = mobs.getSwarm(i).getProjectiles().size();
 					for(int b = 0; b < temp; b++){
